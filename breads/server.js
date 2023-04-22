7ad50c40cd634ae6e1045824505554a9ad9e70de
Christian Bell <camaribell@yahoo.com>
2023-04-22 10:38:08 -0700

    delete route complete

--- breads/server.js
+++ breads/server.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
-
+const methodOverride = require("method-override");
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -12,6 +12,9 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.urlencoded({ extended: true }));
+
+// MIDDLEWARE
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get("/", (req, res) => {
--- breads/views/show.jsx
+++ breads/views/show.jsx
@@ -1,17 +1,23 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
-        <Default>
-          <h2>Show Page</h2>
-          <h3>{bread.name}</h3>
-          <img src={bread.image} alt={bread.name} />
-        </Default>
-      )
-  }
-  
+function Show({ bread, index }) {
+  // Confirm we are getting our bread data in the terminal.
+  // console.log(bread.name)
+  return (
+    <Default>
+      <h2>Show Page</h2>
+      <h3>{bread.name}</h3>
+      <img src={bread.image} alt={bread.name} />
+
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE" />
+      </form>
+    </Default>
+
+  )
+}
+
 
 module.exports = Show
+
