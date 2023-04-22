1f69fbdadaf91c335fb0d886d666e25804e33361
Christian Bell <camaribell@yahoo.com>
2023-04-22 11:55:46 -0700

    edit and update routes complete

--- breads/controllers/breads_controller.js
+++ breads/controllers/breads_controller.js
@@ -2,7 +2,6 @@ const express = require("express");
 const breads = express.Router();
 const Bread = require("../models/bread.js");
 
-
 // INDEX
 breads.get("/", (req, res) => {
   res.render("Index", {
@@ -14,6 +13,14 @@ breads.get("/", (req, res) => {
 breads.get("/new", (req, res) => {
   res.render("new");
 });
+// EDIT
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray,
+  });
+});
+
 // SHOW
 breads.get("/:arrayIndex", (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -45,6 +52,16 @@ breads.post("/", (req, res) => {
 breads.delete("/:indexArray", (req, res) => {
   Bread.splice(req.params.indexArray, 1);
   res.status(303).redirect("/breads");
+});
+// UPDATE
+breads.put("/:arrayIndex", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread[req.params.arrayIndex] = req.body;
+  res.redirect(`/breads/${req.params.arrayIndex}`);
 });
 
 module.exports = breads;
--- breads/views/show.jsx
+++ breads/views/show.jsx
@@ -6,16 +6,24 @@ function Show({ bread, index }) {
   // console.log(bread.name)
   return (
     <Default>
-      <h2>Show Page</h2>
       <h3>{bread.name}</h3>
+      <p>
+        and it
+        {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
+        have gluten.
+      </p>
       <img src={bread.image} alt={bread.name} />
-
+      <li>
+        <a href="/breads">Go home</a>
+      </li>
+      <a href={`/breads/${index}/edit`}>
+        <button>Edit</button>
+      </a>
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
-        <input type='submit' value="DELETE" />
+        <input type="submit" value="DELETE" />
       </form>
     </Default>
-
-  )
+  );
 }
 
 
